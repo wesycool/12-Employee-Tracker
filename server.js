14718e8624941e563398b8251f4ae131bcd48f10
Yun Sum (Edmund) Wong <edmund.wong@live.ca>
2020-06-23 21:09:39 -0400

    Added Add and Delete function

--- server.js
+++ server.js
@@ -49,34 +49,92 @@ async function mainApp(){
     console.log(fs.readFileSync('welcome.md','utf8'))
 
     const searchData = await searchPrompt()
-    deletePrompt(searchData)
+    const changeData = await inquirer.prompt({
+        message:'What would you want to do?', 
+        type: 'list', 
+        name:'action', 
+        choices: ['Add','Update','Delete'].map(choice => {
+            name = `${choice} ${searchData.search.result}`
+            value = `${choice.toLowerCase()}Prompt(searchData)`
+            return {name,value}
+        })
+    })
+
+    await eval(changeData.action)
+    await db.close()
 }
 
 
-// Initial Prompt Function
+// Initial Search Prompt Function
 async function searchPrompt(){
-    const result = await inquirer.prompt(
-        {message:'Viewing:', type: 'list', name:'search', choices:['Department', 'Role', 'Employee']}
-    )
+    const search = await inquirer.prompt({
+        message:'Viewing:', 
+        type: 'list', 
+        name:'result', 
+        choices:['Department', 'Role', 'Employee']
+    })
 
-    const tableList = await db.query( `SELECT * FROM ${result.search}`)
+    const tableList = await db.query( 'SELECT * FROM ??', [search.result])
     console.table( tableList )
-    return {result, tableList}
+    return {search, tableList}
 }
 
 
 // Delete Prompt Function
-async function deletePrompt(searchData){
-    const objKeys = Object.keys(searchData.tableList[0])
-    const choiceList = searchData.tableList.map(data => `${data[objKeys[0]]}. ${data[objKeys[1]]}`)
+async function deletePrompt(data){
+    const objKeys = Object.keys(data.tableList[0])
+    const choices = data.tableList.map(table => {
+        return `${table[objKeys[0]]}. ${table[objKeys[1]]} ${objKeys[2] == 'last_name' ? table[objKeys[2]] : ''}`
+    })
 
-    const choiceDelete = await inquirer.prompt(
-        {message:`Which ${searchData.result.search} to delete:`, type: 'list', name:'delete', choices: choiceList}
-    )
+    const choiceDelete = await inquirer.prompt({
+        message:`Which ${data.search.result.toLowerCase()} to delete:`, 
+        type: 'list', 
+        name:'delete', 
+        choices
+    })
 
-    console.log(choiceDelete.delete.split('.'))
+    console.log(choiceDelete.delete.split('.')[0])
+    await db.query( 'DELETE FROM ?? WHERE id=?', [data.search.result,choiceDelete.delete.split('.')[0]])
 }
 
+
+// Update Prompt Function
+async function updatePrompt(data){
+    const objKeys = Object.keys(data.tableList[0])
+    const choices = data.tableList.map( function(table){
+        return `${table[objKeys[0]]}. ${table[objKeys[1]]} ${objKeys[2] == 'last_name' ? table[objKeys[2]] : ''}`
+    })
+
+    const choiceDelete = await inquirer.prompt({
+        message:`Which ${data.search.result.toLowerCase()} to update:`,
+        type: 'list',
+        name:'delete',
+        choices
+    })
+
+}
+
+
+// Add Prompt Function
+async function addPrompt(data){
+    const objKeys = Object.keys(data.tableList[0])
+    let newValues = {}
+
+    for( const col of objKeys){
+        if(col == 'id') newValues.id = null
+        else{
+            const input = await inquirer.prompt({
+                message: `Input ${col} value:`,
+                type:'input',
+                name: col
+            })
+            newValues = Object.assign(newValues, input)
+        }
+    }
+
+    await db.query( 'INSERT INTO ?? VALUES(?)', [data.search.result,Object.values(newValues)])
+}
 
 
 mainApp()
